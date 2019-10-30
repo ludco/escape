a396989a06a32117a62cf7da9e8928692680f0d2
Ludivine Coujard <ludcoujard@gmail.com>
2019-10-30 23:27:47 +0100

    dvd almost works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { MatDialogModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LivingroomComponent } from './components/livingroom/livingroom.component';
-import { FrontDirective } from './Shared/front.directive';
+import { FrontDirective } from './shared/front.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LivingroomplaceComponent } from './pages/livingroomplace/livingroomplace.component';
 
