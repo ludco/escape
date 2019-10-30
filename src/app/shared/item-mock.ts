49dc684483f687bb8a74d8f1e23ba28403461fa0
HakimDai <hakim69400@live.com>
2019-10-30 22:41:50 +0100

    item and item-mock creation

---
+++ src/app/shared/item-mock.ts
@@ -0,0 +1,16 @@
+import { Item } from './item';
+
+export const Items: Item[] = [
+    {id: 1, url: '../assets/objectPhotos/a.png'},
+    {id: 2, url: '../assets/objectPhotos/c.png'},
+    {id: 3, url: '../assets/objectPhotos/e.png'},
+    {id: 4, url: '../assets/objectPhotos/h.png'},
+    {id: 5, url: '../assets/objectPhotos/k.png'},
+    {id: 6, url: '../assets/objectPhotos/l.png'},
+    {id: 7, url: '../assets/objectPhotos/m.png'},
+    {id: 8, url: '../assets/objectPhotos/o.png'},
+    {id: 9, url: '../assets/objectPhotos/p.png'},
+    {id: 10, url: '../assets/objectPhotos/s.png'},
+    {id: 11, url: '../assets/objectPhotos/s1.png'},
+    {id: 12, url: '../assets/objectPhotos/y.png'}
+]
